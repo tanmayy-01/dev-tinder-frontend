03d0f5eea6830a87a758d82861dbf75dfccd046c
Tanmay Shende <tanmayshende007@gmail.com>
2024-12-07 12:06:49 +0530

    Tailwind installation and configuration is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-      <h1>Hello Universe!!</h1>
-      
-    </>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
