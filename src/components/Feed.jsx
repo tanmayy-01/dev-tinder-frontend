a6d5754e98e27f1b93940f1ebaa6bda18786adb3
Tanmay Shende <tanmayshende007@gmail.com>
2024-12-11 00:53:04 +0530

    user card and feed page ui is completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,39 @@
+import { useDispatch, useSelector } from "react-redux"
+import UserCard from "./UserCard"
+import { useEffect } from "react";
+import { addFeed } from "../utils/feedSlice";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
 
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed)
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "user/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    } catch (err) {
+      //TODO: handle error
+    }
+  };
+  useEffect(() => {
+    getFeed();
+  }, []);
+  if (!feed) return;
+
+  if (feed.length <= 0)
+    return <h1 className="flex justify-center my-10">No new users founds!</h1>;
   return (
-    <div>
-      Feed
-    </div>
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed[0]} />
+      </div>
+    )
   )
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,15 @@
+import { useSelector } from "react-redux";
+import EditProfile from "./EditProfile";
+
 const Profile = () => {
+  const user = useSelector((store) => store.user);
   return (
-    <div>
-      Profile
-    </div>
-  )
+    user && (
+      <div className="h-[60vh] overflow-y-auto">
+        <EditProfile user={user} />
+      </div>
+    )
+  );
 }
 
 export default Profile
